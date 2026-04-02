2e4e63d848bf3e44fed64ce1eff9ccc80b250d5d
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-02 08:25:34 -0300

    feat: added react hook form and tanstack libraries,minor fix in componentes and added input components

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -3,8 +3,19 @@ import { type JSX } from "react";
 interface CardProps {
   children: React.ReactNode;
   className?: string;
+  scale?: boolean;
 }
 
-export function Card({ className, children }: CardProps): JSX.Element {
-  return <div className={`${className} rounded-xl flex flex-col justify-center hover:scale-105  transform transition-transform duration-300 bg-elevated dark:bg-secondary shadow-2xl`}>{children}</div>;
+export function Card({
+  className,
+  children,
+  scale = true,
+}: CardProps): JSX.Element {
+  return (
+    <div
+      className={`${className} ${scale && "hover:scale-105  transform transition-transform duration-300"} rounded-xl flex flex-col justify-center  bg-elevated dark:bg-secondary shadow-2xl`}
+    >
+      {children}
+    </div>
+  );
 }
--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import Typography from "./typography";
+import {
+  FieldErrors,
+  FieldValues,
+  Path,
+  UseFormRegister,
+  get,
+} from "react-hook-form";
 
-interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+interface InputProps<
+  T extends FieldValues,
+> extends React.HTMLAttributes<HTMLInputElement> {
+  register: UseFormRegister<T>;
+  errors: FieldErrors<T>;
+  name: Path<T>;
+  label: string;
+}
 
-const Input = ({}: InputProps) => {
-  return <input />;
-};
+export default function Input<T extends FieldValues>({
+  name,
+  label,
+  register,
+  errors,
+}: InputProps<T>) {
+  const error = get(errors, name);
 
-export default Input;
+  return (
+    <div className="flex flex-col h-15 space-y-2">
+      <Typography as="span" type="body" className="capitalize">
+        {label}
+      </Typography>
+      <input
+        className="p-2.5 rounded-xl bg-slate-500"
+        type="text"
+        {...register(name, {
+          required: true,
+        })}
+      />
+      {error && (
+        <Typography as="span" type="body" className="text-error capitalize">
+          {error.message as string}
+        </Typography>
+      )}
+    </div>
+  );
+}
