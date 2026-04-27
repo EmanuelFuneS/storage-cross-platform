0a5ff3ebcfabe0a96608f7c9fb1dda27469ee987
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-27 02:20:11 -0300

    fix: env folder from main branch

---
+++ packages/env/src/index.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+const schemaEnv = z.object({
+  //environment
+  NODE_ENV: z.string().default("development"),
+  //db credentials
+  DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_USER: z.string(),
+  DB_NAME: z.string(),
+  //web
+
+  PRESIGNED_LAMBDA_URL: z.string(),
+  AUTH_SECRET: z.string(),
+
+  DATABASE_URL: z.string(),
+});
+
+const result = schemaEnv.safeParse(process.env);
+
+if (!result.success && process.env.NODE_ENV !== "production") {
+  console.error("❌ Invalid environment variables:", result.error.format());
+}
+
+export const globalEnv = result.success
+  ? result.data
+  : (process.env as unknown as z.infer<typeof schemaEnv>);
+
+export default globalEnv;
