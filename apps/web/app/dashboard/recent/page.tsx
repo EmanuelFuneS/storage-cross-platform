98ecbcc20f944605cf27b1fbb913e6237ea626b6
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-30 19:44:22 -0300

    feat: added recent and trash pages

--- apps/web/app/dashboard/recent/page.tsx
+++ apps/web/app/dashboard/recent/page.tsx
@@ -1,8 +1,21 @@
-import { Typography } from "@workspace/ui/components";
+"use client";
+import useGetRecentsFiles from "@/lib/hooks/useGetRecentsFiles";
+import { RecentFile } from "@/lib/types/schema.db";
+import { Card, Typography } from "@workspace/ui/components";
+import Image from "next/image";
 import React from "react";
+
+const distUrl = `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}/`;
 
 const Page = () => {
   const cards = new Array(10).fill("");
+
+  const { data } = useGetRecentsFiles();
+
+  console.log(data);
+
+  if (!data) return <>...Loading</>;
+
   return (
     <div>
       <section className="my-10">
@@ -15,9 +28,24 @@ const Page = () => {
           </Typography>
         </div>
       </section>
-      <section className="w-full h-190 flex flex-wrap overflow-y-scroll">
-        {cards.map((el, idx) => (
-          <div key={idx} className="w-60 h-60 bg-orange-300 m-5">{el}</div>
+      <section className="w-full h-190 flex flex-wrap overflow-y-scroll gap-4 py-5 px-2">
+        {data.map((el: RecentFile, idx: number) => (
+          <Card key={idx} className="w-70 h-70 flex flex-col items-center justify-start space-y-4">
+            <div className="m-2">
+              <Typography as="p" type="body">
+                {el.File.name}
+              </Typography>
+            </div>
+            <div className="max-h-60 w-full">
+              <Image
+                src={distUrl + el.File.s3_key}
+                alt=""
+                width={100}
+                height={100}
+                className="object-center w-full h-full rounded-b-xl"
+              />
+            </div>
+          </Card>
         ))}
       </section>
     </div>
--- apps/web/app/dashboard/trash/page.tsx
+++ apps/web/app/dashboard/trash/page.tsx
@@ -1,8 +1,23 @@
-import { Button, Typography } from "@workspace/ui/components";
-import { Trash } from "@workspace/ui/lib";
+"use client";
 import React from "react";
+import { Button, Card, Typography } from "@workspace/ui/components";
+import { Trash } from "@workspace/ui/lib";
+import { File } from "@/lib/types/schema.db";
+import Image from "next/image";
+import useGetDeletedFiles from "@/lib/hooks/useGetDeletedFiles";
+
+const distUrl = `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN}/`;
+
+//here delete file in s3
+
+//here handle restore file
 
 const Page = () => {
+
+  const { data } = useGetDeletedFiles();
+
+  if (!data) return <>...loding</>;
+
   return (
     <div className="w-full h-full">
       <section className="my-10 flex justify-between">
@@ -23,8 +38,28 @@ const Page = () => {
           </Button>
         </div>
       </section>
-      <section className="bg-orange-500 w-full h-4/5">
-        items
+      <section className="w-full h-190 flex flex-wrap overflow-y-scroll gap-4 py-5 px-2">
+        {data.map((el: File, idx: number) => (
+          <Card
+            key={idx}
+            className="w-70 h-70 flex flex-col items-center justify-start space-y-4"
+          >
+            <div className="m-2">
+              <Typography as="p" type="body">
+                {el.name}
+              </Typography>
+            </div>
+            <div className="max-h-60 w-full">
+               <Image
+                src={distUrl + el.s3_key}
+                alt=""
+                width={100}
+                height={100}
+                className="object-center w-full h-full rounded-b-xl"
+              />
+            </div>
+          </Card>
+        ))}
       </section>
     </div>
   );
