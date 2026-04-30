bef7a4f3075a87c00f085e7e7629f4f114992fbe
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-30 19:45:35 -0300

    feat: next image config and env vars

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   transpilePackages: ["@repo/env"],
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
