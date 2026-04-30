eed32a4167cdf9b320d702b4e934c10f31ab3a55
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-30 15:27:33 -0300

    feat: added recents files hooks for get and post

---
+++ apps/web/lib/hooks/useAddRecentFile.tsx
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const useAddRecentFile = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["recentFile"],
+    mutationFn: async (data: { fileId: string }) => {
+      const res = await fetch("/api/files", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((r) => r.json());
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["recentsFiles"] });
+    },
+  });
+};
+
+export default useAddRecentFile;
