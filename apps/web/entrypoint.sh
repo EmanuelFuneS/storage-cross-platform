f59ec8079132f6c1007eeede1a7260de5b4203eb
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-05-10 12:52:16 -0300

    update: entry point for migration

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -6,9 +6,9 @@ export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT
 echo "DATABASE_URL: postgresql://${DB_USER}:****@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 echo "Run Drizzle Migrations"
-#cd apps/web
-#npx drizzle-kit migrate --config=./drizzle.config.ts
-#cd /app
+cd apps/web
+npx drizzle-kit migrate --config=./drizzle.config.ts
+cd /app
 
 echo "Run APP"
 exec "$@"
