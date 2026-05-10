7d582c6429f66bb442ef5bcd9e678c1e3ccc0db1
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-05-10 20:13:39 -0300

    update

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -5,10 +5,11 @@ export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT
 
 echo "DATABASE_URL: postgresql://${DB_USER}:****@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require"
 
-#echo "Run Drizzle Migrations"
-#cd apps/web
-#drizzle-kit migrate --config=./drizzle.config.ts && echo "Migrations OK"
-#cd /app
+echo "Run Drizzle Migrations"
+cd apps/web
+NODE_TLS_REJECT_UNAUTHORIZED=0 drizzle-kit migrate --config=./drizzle.config.ts
+echo "Migrations OK"
+cd /app
 
 echo "Run APP"
 exec "$@"
