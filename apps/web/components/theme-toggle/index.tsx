e9f43e8bdaa5be2f0e046e461c8fb6cc1232553f
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-05-19 11:01:37 -0300

    fix: theme toggle

--- apps/web/components/theme-toggle/index.tsx
+++ apps/web/components/theme-toggle/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Moon, SunDim } from "@workspace/ui/lib";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 interface ThemeToggleProps {
@@ -7,30 +8,17 @@ interface ThemeToggleProps {
 }
 
 const ThemeToggle = ({ size }: ThemeToggleProps) => {
-  const [theme, setTheme] = useState<string | null>(null);
+  const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    const saveTheme = localStorage.getItem("theme");
-    if (saveTheme) {
-      setTheme(saveTheme);
-      setMounted(true);
-    } else {
-      setTheme("light");
-    }
+    setMounted(true);
   }, []);
 
-  const changeTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-    document.documentElement.classList.toggle("dark");
-  };
-
-  if (!mounted) return <div className="w-9.25 h-9.25">Loading</div>;
+  if (!mounted) return <div className="w-9.25 h-9.25" />;
 
   return (
-    <div onClick={changeTheme} className="">
+    <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
       {theme === "dark" ? (
         <Moon
           size={size || 25}
