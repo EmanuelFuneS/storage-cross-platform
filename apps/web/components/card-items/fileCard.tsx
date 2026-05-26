7f88cf04d8515a15d9043fba5e1365c569e81f7a
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-05-26 12:48:45 -0300

    feat: add file overlay info in FilesCard

--- apps/web/components/card-items/fileCard.tsx
+++ apps/web/components/card-items/fileCard.tsx
@@ -10,6 +10,7 @@ import {
 import { File } from "@/lib/types/schema.db";
 import useRestoreFile from "@/lib/hooks/useRestoreFile";
 import useHardDeleteFile from "@/lib/hooks/useHardDeleteFile";
+import FileHelper from "@/lib/utils/FileHelper";
 
 const FilePreview = dynamic(
   () =>
@@ -52,9 +53,13 @@ const FilesCard = ({ data, option }: FileCardProps) => {
   };
 
   return (
-    <div className={`relative h-80 rounded-xl overflow-hidden flex flex-col items-center justify-start ${isVideo ? "w-[31rem]" : "w-60"}`}>
+    <div
+      className={`relative h-80 rounded-xl overflow-hidden flex flex-col items-center justify-start ${isVideo ? "w-[31rem]" : "w-60"}`}
+    >
       <div className="p-1 flex w-full items-center justify-between">
-        <div className={`absolute top-5 left-3 z-50 ${option && "flex justify-between w-18"}`}>
+        <div
+          className={`absolute top-5 left-3 z-50 ${option && "flex justify-between w-18"}`}
+        >
           <Star
             size={20}
             className={`${data.is_starred ? "text-yellow-300" : ""}`}
@@ -68,7 +73,20 @@ const FilesCard = ({ data, option }: FileCardProps) => {
         </div>
       </div>
       <div className="h-auto w-full">
-        {distUrl && <FilePreview type={data?.type?.name} file={data} compact={true} />}
+        {distUrl && (
+          <FilePreview type={data?.type?.name} file={data} compact={true} />
+        )}
+
+        {data.type.name !== "audio" ? (
+          <div className="absolute bottom-0 left-0 z-50 pointer-events-none text-elevated bg-transparent  text-xs lg:text-xl flex items-center justify-center w-full">
+            <Typography as="span" type="body">
+              {data.name.slice(0, 15)} . . .{" "}
+              {FileHelper.formatSize(Number(data.size))}
+            </Typography>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
